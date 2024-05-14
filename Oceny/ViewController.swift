0ef813a80ced8d31d454253f52fb47779bd75ef5
Pawciuz <podgorskipawel16@gmail.com>
2024-05-14 09:25:41 +0200

    Initial Commit

---
+++ Oceny/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Oceny
+//
+//  Created by Paweł on 14/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
